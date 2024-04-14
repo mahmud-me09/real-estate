aaabb0a1f26a34977b63dbc2c08c28c522958605
mahmud-me09 <mahmud.me09@gmail.com>
2024-04-14 20:27:06 +0600

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+
+
+
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<>
+				<div className="btn btn-primary text-4xl">Hello world!</div>
+				<App></App>
+			</>
+		),
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
